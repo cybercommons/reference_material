81c0d66d66227f9d8b2c8ab0fec06b63d154ed27
Andrew Rocha <sportman076@gmail.com>
2017-02-22 15:45:24 -0600

    deleted practice files

--- reading1.py
+++
@@ -1,9 +0,0 @@
-sentence = input("enter a sentence")
-list_of_words = sentence.split()
-print("there are", len(list_of_words), "words.")
-print(list_of_words)
-sum = 0
-for word in list_of_words:
-    sum = sum + len(word)
-
-print("the average word length is", sum/len(list_of_words) )
